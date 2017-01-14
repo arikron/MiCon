d12b8763ab8947e9483d40dfb8b5d56561dfe035
daze <d.zelosko@gmail.com>
2017-01-14 14:44:36 +0100

    Blub

--- AndroidStudioProjects/betterMQTTcielnt/app/src/main/java/eu/micom/bettermqttcielnt/MainActivity.java
+++ AndroidStudioProjects/betterMQTTcielnt/app/src/main/java/eu/micom/bettermqttcielnt/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 //////////////////////BLAAAAAAAAAAAAAAAAAAAAAAAAA
+        //////BLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
     tv.setText(stringFromJNI());
